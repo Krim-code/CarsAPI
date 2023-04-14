65afaa9bcddcd4f1df8158b1910d4ea6eefa40e1
Krim-code <suhetsky.gricha555@gmail.com>
2023-04-14 16:35:40 +0300

    Corrected cars/urls

--- cars/api/urls.py
+++ cars/api/urls.py
@@ -3,12 +3,12 @@ from pprint import pprint
 from django.urls import path
 from rest_framework.routers import SimpleRouter
 
-from .views import index, CountryViewSet, ManufacturerViewSet, CarViewSet, CommentViewSet
+from .views import CountryViewSet, ManufacturerViewSet, CarViewSet, CommentViewSet
 
 router = SimpleRouter()
 router.register(r'countries', CountryViewSet, basename="countries")
 router.register(r'manufacturers', ManufacturerViewSet, basename="manufacturers")
 router.register(r'cars', CarViewSet, basename="cars")
 router.register(r'comments', CommentViewSet, basename="comments")
-pprint(router.urls)
+
 urlpatterns = router.urls
