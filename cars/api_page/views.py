569ec72cda729fd42f5c9b487f9c83115985b22f
Krim-code <suhetsky.gricha555@gmail.com>
2023-04-14 22:55:03 +0300

    Start Creating Api Page

--- cars/api_page/views.py
+++ cars/api_page/views.py
@@ -1,8 +1,12 @@
 from django.http import HttpResponse
 from django.shortcuts import render
+from django.template import loader
 
 
 # Create your views here.
 
 def index(request, *args, **kwargs):
-    return HttpResponse("DDDDDD")
+    template = loader.get_template('index.html')
+    return render(request, 'index.html')
+
+
